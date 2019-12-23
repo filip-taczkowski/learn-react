3685695cf5189e12514b76822df641333c469843
Filip Taczkowski <filip.taczkowski@gmail.com>
2019-12-23 18:00:38 +0100

    Add new component - Column

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,9 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello World!</h2>
-        <List />
+        <List title={['Interesting thing I want to check out ', <sup key='1'>soon!</sup>]} image='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'>
+
+        </List>
       </main>
     )
   }
